22a3911adaf50c8d5a21e842e4724892378f068c
Rikin Marfatia <rikin@supergooey.dev>
2024-08-31 14:24:12 -0700

    Added a way to chat with Pical in case any corrections are needed (#7)

--- app/src/main/java/dev/supergooey/caloriesnap/CameraFeature.kt
+++ app/src/main/java/dev/supergooey/caloriesnap/CameraFeature.kt
@@ -17,7 +17,9 @@ import androidx.compose.animation.SharedTransitionLayout
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.EaseInOut
 import androidx.compose.animation.core.EaseInOutQuint
+import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDp
+import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.core.updateTransition
 import androidx.compose.animation.fadeIn
@@ -38,14 +40,24 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.Send
 import androidx.compose.material.icons.rounded.Check
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -55,11 +67,17 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModel
@@ -171,7 +189,7 @@ fun CameraScreen(
                 modifier = Modifier
                   .sharedElement(
                     state = rememberSharedContentState(key = "image"),
-                    boundsTransform = { initialBounds, targetBounds ->
+                    boundsTransform = { _, _ ->
                       tween(
                         durationMillis = animationDuration,
                         easing = easing
@@ -189,13 +207,21 @@ fun CameraScreen(
 
         CameraFeatureStep.Analysis -> {
           val rotation = remember { Animatable(0f) }
+          val listState = rememberLazyListState()
+
+          LaunchedEffect(state.messages) {
+            if (listState.layoutInfo.totalItemsCount > 0) {
+              listState.animateScrollToItem(listState.layoutInfo.totalItemsCount-1)
+            }
+          }
+
           LaunchedEffect(Unit) {
             delay(animationDuration.toLong())
             actions(CameraFeature.Action.AnalyzePhoto(state.capturedPhoto!!))
           }
 
-          LaunchedEffect(state.mealResponse) {
-            if (state.mealResponse == null) {
+          LaunchedEffect(state.loading) {
+            if (state.loading) {
               delay(animationDuration.toLong())
               while (true) {
                 rotation.animateTo(
@@ -208,99 +234,148 @@ fun CameraScreen(
                 )
               }
             } else {
-              rotation.animateTo(0f)
+              rotation.animateTo(0f, spring(dampingRatio = Spring.DampingRatioMediumBouncy, stiffness = Spring.StiffnessLow))
             }
           }
 
-          Box(modifier = Modifier.fillMaxSize()) {
-            Column(
+          Column(
+            modifier = Modifier
+              .fillMaxSize()
+              .background(color = Color.Black)
+              .windowInsetsPadding(WindowInsets.statusBars)
+              .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+          ) {
+            Image(
               modifier = Modifier
-                .fillMaxSize()
-                .background(color = Color.Black)
-                .windowInsetsPadding(WindowInsets.statusBars)
-                .padding(16.dp),
-              verticalArrangement = Arrangement.spacedBy(16.dp),
-              horizontalAlignment = Alignment.CenterHorizontally
+                .sharedElement(
+                  state = rememberSharedContentState("image"),
+                  animatedVisibilityScope = this@AnimatedContent,
+                  boundsTransform = { _, _ ->
+                    tween(durationMillis = animationDuration, easing = easing)
+                  },
+                )
+                .graphicsLayer {
+                  rotationZ = rotation.value
+                }
+                .size(300.dp)
+                .clip(RoundedCornerShape(cornerRadius)),
+              bitmap = state.capturedPhoto!!.asImageBitmap(),
+              contentScale = ContentScale.Crop,
+              contentDescription = "Photo"
+            )
+            LazyColumn(
+              modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+              state = listState,
+              verticalArrangement = Arrangement.spacedBy(8.dp)
             ) {
-              Image(
-                modifier = Modifier
-                  .sharedElement(
-                    state = rememberSharedContentState("image"),
-                    animatedVisibilityScope = this@AnimatedContent,
-                    boundsTransform = { initialBounds, targetBounds ->
-                      tween(durationMillis = animationDuration, easing = easing)
-                    },
-                  )
-                  .graphicsLayer {
-                    rotationZ = rotation.value
+              items(items = state.messages) { message ->
+                if (message.content[0] is MessageContent.Text) {
+                  val alignment = if (message.role == "user") Alignment.TopEnd else Alignment.TopStart
+                  val color = if (message.role == "user") MaterialTheme.colorScheme.primaryContainer else Color.White
+                  val textColor = if (message.role == "user") MaterialTheme.colorScheme.onPrimaryContainer else Color.Black
+
+                  val content = message.content[0] as MessageContent.Text
+                  Box(
+                    modifier = Modifier
+                      .fillMaxWidth()
+                      .wrapContentHeight(),
+                    contentAlignment = alignment
+                  ) {
+                    Text(
+                      modifier = Modifier
+                        .fillMaxWidth(0.8f)
+                        .wrapContentHeight()
+                        .clip(RoundedCornerShape(8.dp))
+                        .background(color = color)
+                        .padding(8.dp),
+                      text = content.text,
+                      color = textColor
+                    )
                   }
-                  .size(300.dp)
-                  .clip(RoundedCornerShape(cornerRadius)),
-                bitmap = state.capturedPhoto!!.asImageBitmap(),
-                contentScale = ContentScale.Crop,
-                contentDescription = "Photo"
-              )
-              AnimatedVisibility(state.mealResponse != null) {
+                }
+              }
+            }
+            AnimatedVisibility(state.mealResponse?.valid == true) {
+              Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
+                Text(
+                  text = "Add some context",
+                  style = TextStyle(
+                    color = Color.White,
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.Medium
+                  )
+                )
+                BasicTextField(
+                  value = state.contextMessage,
+                  onValueChange = { actions(CameraFeature.Action.UpdateContextMessage(it)) },
+                  textStyle = LocalTextStyle.current.copy(color = MaterialTheme.colorScheme.onPrimaryContainer),
+                  keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Go
+                  ),
+                  keyboardActions = KeyboardActions(onGo = { actions(CameraFeature.Action.SendContextMessage) }),
+                  cursorBrush = SolidColor(value = MaterialTheme.colorScheme.onPrimaryContainer)
+                ) { innerTextField ->
+                  Row(
+                    modifier = Modifier
+                      .fillMaxWidth()
+                      .wrapContentHeight()
+                      .clip(
+                        RoundedCornerShape(8.dp)
+                      )
+                      .background(color = MaterialTheme.colorScheme.primaryContainer)
+                      .padding(vertical = 16.dp, horizontal = 8.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(4.dp)
+                  ) {
+                    Box(modifier = Modifier.weight(1f)) {
+                      innerTextField()
+                    }
+                    Icon(
+                      modifier = Modifier
+                        .size(24.dp)
+                        .clickable { actions(CameraFeature.Action.SendContextMessage) },
+                      imageVector = Icons.AutoMirrored.Rounded.Send,
+                      tint = MaterialTheme.colorScheme.onPrimaryContainer,
+                      contentDescription = "Send Context Message"
+                    )
+                  }
+                }
+              }
+            }
+            Row(
+              modifier = Modifier.fillMaxWidth(),
+              horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterHorizontally),
+              verticalAlignment = Alignment.CenterVertically
+            ) {
+              AnimatedVisibility(state.mealResponse?.valid == true) {
                 Box(
                   modifier = Modifier
-                    .align(Alignment.Start)
                     .wrapContentSize()
-                    .clip(
-                      RoundedCornerShape(
-                        topEnd = 8.dp,
-                        bottomEnd = 8.dp,
-                        topStart = 8.dp
-                      )
-                    )
+                    .clip(RoundedCornerShape(8.dp))
                     .background(color = Color.White)
                     .padding(8.dp)
                 ) {
                   Text(
-                    modifier = Modifier
-                      .clip(
-                        RoundedCornerShape(
-                          topEnd = 8.dp,
-                          bottomEnd = 8.dp,
-                          topStart = 8.dp
-                        )
-                      )
-                      .background(color = Color.White)
-                      .align(Alignment.Center),
-                    text = state.mealResponse?.foodDescription?.trim() ?: "",
+                    text = "Calories: ${state.mealResponse?.totalCalories}",
                     color = Color.Black
                   )
                 }
               }
-              Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterHorizontally),
-                verticalAlignment = Alignment.CenterVertically
-              ) {
-                AnimatedVisibility(state.mealResponse?.valid == true) {
-                  Box(
-                    modifier = Modifier
-                      .wrapContentSize()
-                      .clip(RoundedCornerShape(8.dp))
-                      .background(color = Color.White)
-                      .padding(8.dp)
-                  ) {
-                    Text(
-                      text = "Calories: ${state.mealResponse?.totalCalories}",
-                      color = Color.Black
-                    )
-                  }
-                }
-                AnimatedVisibility(state.mealResponse?.valid == true) {
-                  Button(
-                    onClick = { actions(CameraFeature.Action.LogMeal) },
-                    colors = ButtonDefaults.buttonColors(containerColor = CoolGreen)
-                  ) {
-                    Icon(
-                      modifier = Modifier.size(24.dp),
-                      imageVector = Icons.Rounded.Check,
-                      contentDescription = "Save"
-                    )
-                  }
+              AnimatedVisibility(state.mealResponse?.valid == true) {
+                Button(
+                  onClick = { actions(CameraFeature.Action.LogMeal) },
+                  colors = ButtonDefaults.buttonColors(containerColor = CoolGreen)
+                ) {
+                  Icon(
+                    modifier = Modifier.size(24.dp),
+                    imageVector = Icons.Rounded.Check,
+                    contentDescription = "Save"
+                  )
                 }
               }
             }
@@ -321,13 +396,18 @@ interface CameraFeature {
   data class State(
     val capturedPhoto: Bitmap? = null,
     val mealResponse: MealResponse? = null,
-    val step: CameraFeatureStep = CameraFeatureStep.Camera
+    val messages: List<Message> = emptyList(),
+    val contextMessage: String = "",
+    val step: CameraFeatureStep = CameraFeatureStep.Camera,
+    val loading: Boolean = false
   )
 
-  sealed class Action {
-    data class TakePhoto(val bitmap: Bitmap) : Action()
-    data class AnalyzePhoto(val bitmap: Bitmap) : Action()
-    data object LogMeal : Action()
+  sealed interface Action {
+    data class TakePhoto(val bitmap: Bitmap) : Action
+    data class AnalyzePhoto(val bitmap: Bitmap) : Action
+    data object LogMeal : Action
+    data class UpdateContextMessage(val message: String): Action
+    data object SendContextMessage: Action
   }
 }
 
@@ -353,46 +433,51 @@ class CameraViewModel(
       }
 
       is CameraFeature.Action.AnalyzePhoto -> {
-        val request = MessagesRequest(
-          messages = listOf(
-            Message(
-              role = "user",
-              content = listOf(
-                MessageContent.Image(
-                  source = ImageSource(
-                    data = action.bitmap.toBase64()
-                  )
+        internalState.update { it.copy(loading = true) }
+        val messages = mutableListOf<Message>()
+        val initialMessage =
+          Message(
+            role = "user",
+            content = listOf(
+              MessageContent.Image(
+                source = ImageSource(
+                  data = action.bitmap.toBase64()
                 )
               )
             )
           )
-        )
-
+        messages.add(initialMessage)
         viewModelScope.launch {
           // Send it to Claude
-          val response = ImageToCalorieClient.api.getMessages(request)
+          val response = ImageToCalorieClient.api.getMessages(MessagesRequest(messages = messages))
           Log.d("Camera", "Claude Response: $response")
 
           if (response.isSuccessful) {
+            val messagesResponse = response.body()!!
             val meal = Json.decodeFromString<MealResponse>(
-              response.body()!!
+              messagesResponse
                 .content
                 .filterIsInstance<MessageContent.Text>()
                 .first()
                 .text
             )
+            val mealMessage = Message(
+              role = messagesResponse.role,
+              content = listOf(
+                MessageContent.Text(text = meal.foodDescription)
+              )
+            )
+            messages.add(mealMessage)
             internalState.update { current ->
               current.copy(
-                mealResponse = meal
+                loading = false,
+                mealResponse = meal,
+                messages = messages.toList()
               )
             }
           } else {
             Log.d("Camera", "Claude Error Response: ${response.errorBody()?.string()}")
-            /*internalState.update { current ->
-                current.copy(
-                    messages = listOf(MessageContent.Text("Oops. Something went wrong"))
-                )
-            }*/
+            internalState.update { current -> current.copy( loading = false, ) }
           }
         }
       }
@@ -404,6 +489,56 @@ class CameraViewModel(
           db.mealLogDao().insertMealLog(log)
           navController.navigate("home")
         }
+      }
+
+      CameraFeature.Action.SendContextMessage -> {
+        viewModelScope.launch {
+          val message = Message(
+            role = "user",
+            content = listOf(
+              MessageContent.Text(text = internalState.value.contextMessage)
+            )
+          )
+          val messages = internalState.value.messages.toMutableList().apply {
+            add(message)
+          }
+          internalState.update { it.copy(loading = true, contextMessage = "", messages = messages.toList()) }
+
+          val response = ImageToCalorieClient.api.getMessages(MessagesRequest(messages = messages))
+
+          if (response.isSuccessful) {
+            val messagesResponse = response.body()!!
+            Log.d("Camera", "Claude Context Response: $messagesResponse")
+            val meal = Json.decodeFromString<MealResponse>(
+              messagesResponse
+                .content
+                .filterIsInstance<MessageContent.Text>()
+                .first()
+                .text
+            )
+            val mealMessage = Message(
+              role = messagesResponse.role,
+              content = listOf(
+                MessageContent.Text(text = meal.foodDescription)
+              )
+            )
+            messages.add(mealMessage)
+            internalState.update { current ->
+              current.copy(
+                loading = false,
+                mealResponse = meal,
+                messages = messages.toList()
+              )
+            }
+          } else {
+            Log.d("Camera", "Claude Error Response: ${response.errorBody()?.string()}")
+            internalState.update { it.copy(loading = false) }
+          }
+        }
+      }
+
+      is CameraFeature.Action.UpdateContextMessage -> {
+        internalState.update { it.copy(contextMessage = action.message) }
       }
     }
   }
@@ -422,6 +557,13 @@ class CameraViewModel(
       return CameraViewModel(store, db) as T
     }
   }
+}
+
+fun MessagesResponse.toMessage(): Message {
+  return Message(
+    role = role,
+    content = content
+  )
 }
 
 fun MealResponse.toMealLog(
